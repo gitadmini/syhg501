9ac2e69d305a781d572fd1019ae29bb1567bb02d
linyy <457938831@qq.com>
2018-12-10 11:08:53 +0800

    1

--- src/main/java/linyy/hz/syhg501/art/gif/GifMain.java
+++ src/main/java/linyy/hz/syhg501/art/gif/GifMain.java
@@ -15,17 +15,15 @@ public class GifMain {
     public static void main(String[] args) throws IOException {
         List<File> list = new ArrayList<File>();
         File files = new File("f:\\temp"); // 文件夾
-        File out = new File(
-            "f:\\temp\\"
-                    + new Date().getTime() + ".gif");
+        File out = new File("f:\\temp\\" + new Date().getTime() + ".gif");
         for (File file : files.listFiles()) {
             list.add(file);
         }
         Collections.sort(list);
-//        Collections.reverse(list);
+        // Collections.reverse(list);
         FileOutputStream fos = new FileOutputStream(out);
         AnimatedGifEncoder e = new AnimatedGifEncoder();
-//        e.setRepeat(1);
+        // e.setRepeat(1);
         e.start(fos);
         e.setDelay(50);
         for (File file : list) {
